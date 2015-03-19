f2ff272b0fc4db8600d77aa650e67f7ae6c0909c
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2015-03-18 23:47:36 -0400

    Edits to metapopversion, rebuild of package.

--- R/metapopversion.r
+++ R/metapopversion.r
@@ -1,23 +1,25 @@
+#' @export metapopversion
+#' @title Determine RAMAS Metapop version used to create a *.MP file
+#' @description \code{metapopversion} is a function called from within the
+#' \code{mp.read} function. It's single purpose is to read the `list` object
+#' that contains all of the *.MP file values and determine which version
+#' of RAMAS Metapop was used to construct the file.
+#'
+#' @param mpFile A list object of all of the parameters set in a *.MP file
+#'
+#'@return RAMAS Metapop version number as an integer
+#'
 metapopversion <- function(mpFile) {
-	# Reads a 'list' variable that is composed of all lines of a *.mp file, as used
-	# in the RAMAS Metapop program, and returns the Metapop version number as an integer.
-	#
-	# Args:
-	#	mpFile: a 'list' variable that is composed of all of the lines of a *.mp file
-	#
-	# Returns:
-	#	RAMAS Metapop version number, as an integer
-	#
-	# Author: Matthew Aiello-Lammens
-	# Date: 2 February 2011
-	
-	verLine <- mpFile[1]
-	verLine <- unlist(strsplit(verLine,' '))
-	if (verLine[1] == "Metapopulation") {
-		version <- verLine[4] # Version number should be the fourth element
-		version <- gsub("[^0-9]","",version) # Remove non-numeric values
-		return(version)
-	} else {
-		stop("Error: *.mp File is not a RAMAS Metapop File")
-	}
+  # Author: Matthew Aiello-Lammens
+  # Date: 2 February 2011
+
+  verLine <- mpFile[1]
+  verLine <- unlist(strsplit(verLine,' '))
+  if (verLine[1] == "Metapopulation") {
+    version <- verLine[4] # Version number should be the fourth element
+    version <- gsub("[^0-9]","",version) # Remove non-numeric values
+    return(version)
+  } else {
+    stop("Error: *.mp File is not a RAMAS Metapop File")
+  }
 }
